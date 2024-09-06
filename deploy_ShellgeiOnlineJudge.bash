d7be7c2c8d595bfc313822756f15a6ca22cffdb1
YusukeKato <takenoko2tomato2+github@gmail.com>
2024-09-06 13:24:43 +0900

    update

--- deploy_ShellgeiOnlineJudge.bash
+++ deploy_ShellgeiOnlineJudge.bash
@@ -52,10 +52,4 @@ sudo cp ShellgeiOnlineJudgeServer/run_shellgei.py "$root_path"
 sudo cp ShellgeiOnlineJudgeServer/z.bash "$root_path"
 
 # chmod
-sudo chmod 644 "$root_path_html"connection.php
-sudo chmod 644 "$root_path_html"problem_jp
-sudo chmod 644 "$root_path_html"problem_en
-sudo chmod 644 "$root_path_html"problem_images
-sudo chmod 644 "$root_path_html"input
-sudo chmod 644 "$root_path_html"output
 sudo chmod 777 "$root_path"z.bash
