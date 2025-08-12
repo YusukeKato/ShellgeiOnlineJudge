1e87ea6830813d63166e87b4903a4553fc31d7d0
YusukeKato <yusukekato.contact@gmail.com>
2025-08-12 14:21:45 +0900

    eslintの設定をreact用に修正する試み

--- client/soj-react-pkg/eslint.config.js
+++ client/soj-react-pkg/eslint.config.js
@@ -1,11 +1,40 @@
-import { defineConfig } from "eslint/config";
+import defineConfig from "eslint/config";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
 
 export default defineConfig([
+    {
+        languageOptions: {
+          globals: {
+            ...globals.browser,
+            ...globals.node,
+          },
+          parserOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            ecmaFeatures: {
+              jsx: true,
+            },
+          },
+        },
+    },
+    pluginJs.configs.recommended,
 	{
 		files: ["**/*.js", "**/*.cjs", "**/*.mjs", "**/*.jsx"],
+        plugins: {
+          react: pluginReact,
+          prettier: pluginPrettier,
+        },
 		rules: {
 			"prefer-const": "warn",
 			"no-constant-binary-expression": "error",
+            "react/react-in-jsx-scope": "off",
+			...pluginReact.configs.recommended.rules,
+			"react/jsx-uses-react": "error",
+			"react/jsx-uses-vars": "error",
+			"prettier/prettier": "error",
 		},
 	},
 ]);
