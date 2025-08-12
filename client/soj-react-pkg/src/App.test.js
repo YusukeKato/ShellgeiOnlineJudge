38c7b55ece8703a9c589a086adac15e85a9b6e85
YusukeKato <yusukekato.contact@gmail.com>
2025-08-12 16:43:23 +0900

    add test

--- client/soj-react-pkg/src/App.test.js
+++ client/soj-react-pkg/src/App.test.js
@@ -2,8 +2,14 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App.tsx";
 
-test("renders learn react link", () => {
+test("renders header", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/シェル・ワンライナーの遊び場/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders footer", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/2023 YusukeKato All rights reserved./i);
   expect(linkElement).toBeInTheDocument();
 });
